0e91aaa018782cd13d8e20e629152bb815b5f1b8
not-my-circus <hollis.mccray@gmail.com>
2025-03-05 15:16:18 -0700

    First public site build

--- src/generate_page.py
+++ src/generate_page.py
@@ -2,11 +2,9 @@ from markdown_to_html import markdown_to_html_node
 from pathlib import Path
 import os, shutil
 
-def copy_static():
-    src_dir = "static"
-    dest_dir  = "public"
+def copy_static(static_dir, dest_dir):
 
-    src_path = os.path.join(os.curdir, src_dir)
+    src_path = os.path.join(os.curdir, static_dir)
     dest_path = os.path.join(os.curdir, dest_dir)
 
     if os.path.exists(dest_path):
@@ -38,7 +36,7 @@ def extract_title(markdown):
 
     raise Exception("title not found")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, BASEPATH):
     print(f"generating page from {from_path} to {dest_path} using {template_path}")
     with open(os.path.join(os.curdir,from_path)) as source_file:
         source = source_file.read()
@@ -50,11 +48,13 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown=source)
     html = template.replace("{{ Title }}", title)
     html = html.replace("{{ Content }}", content)
+    html = html.replace('href="/', 'href="{BASEPATH}')
+    html = html.replace('src="/', 'src="{BASEPATH}')
 
     with open(os.path.join(os.curdir,dest_path), "w") as output:
         output.write(html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, BASEPATH):
     src_list = os.listdir(dir_path_content)
     for item in src_list:
         item_path = os.path.join(dir_path_content,item)
@@ -63,9 +63,9 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             if Path(item_path).suffix == ".md":
                 new_path = os.path.join(dest_dir_path,item)
                 new_path = new_path.replace(".md", ".html")
-                generate_page(item_path, template_path,new_path)
+                generate_page(item_path, template_path,new_path, BASEPATH=BASEPATH)
         else:
             old_dir = os.path.join(dir_path_content, item)
             new_dir = os.path.join(dest_dir_path, item)
             os.mkdir(new_dir)
-            generate_pages_recursive(old_dir, template_path, new_dir)
+            generate_pages_recursive(old_dir, template_path, new_dir, BASEPATH=BASEPATH)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from generate_page import *
+import sys
 
 def main():
-    copy_static()
-    generate_page("content/index.md", template_path="template.html", dest_path="public/index.html")
-    generate_pages_recursive(dir_path_content="content",template_path="template.html", dest_dir_path="public")
+    static_dir = "static"
+    dest_dir = "docs"
+    copy_static(static_dir, dest_dir)
+    if len(sys.argv) >= 2:
+        BASEPATH = sys.argv[1]
+    else:
+        BASEPATH = "/"
+    generate_pages_recursive(dir_path_content="content",template_path="template.html", dest_dir_path=dest_dir, BASEPATH=BASEPATH)
 
 main()
